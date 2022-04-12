c2358c63fb9fb8c6e663b9b96896b70a0729da9d
ksco <numbksco@gmail.com>
2022-04-13 01:58:33 +0800

    Add + and - operators

--- main.c
+++ main.c
@@ -7,9 +7,30 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+  char *p = argv[1];
+
   printf("  .globl main\n");
   printf("main:\n");
-  printf("  li a0,%d\n", atoi(argv[1]));
+  printf("  li a0,%ld\n", strtol(p, &p, 10));
+
+  while (*p) {
+    if (*p == '+') {
+      p++;
+      printf("  addi a0,a0,%ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    if (*p == '-') {
+      p++;
+      printf("  addi a0,a0,-%ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    fprintf(stderr, "unexpected character: '%c'\n", *p);
+    return 1;
+  }
+
+
   printf("  ret\n");
   return 0;
 }
