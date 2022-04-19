741eaeda40c5493cac4577e5f273a3ffe0caf25b
ksco <numbksco@gmail.com>
2022-04-19 23:48:34 +0800

    Support function call with up to 6 arguments

--- codegen.c
+++ codegen.c
@@ -1,6 +1,7 @@
 #include "chibicc.h"
 
 static int depth;
+static char *argreg[] = {"a0", "a1", "a2", "a3", "a4", "a5"};
 
 static void gen_expr(Node *node);
 
@@ -70,9 +71,20 @@ static void gen_expr(Node *node) {
     pop("a1");
     printf("  sd a0,0(a1)\n");
     return;
-  case ND_FUNCALL:
+  case ND_FUNCALL: {
+    int nargs = 0;
+    for (Node *arg = node->args; arg; arg = arg->next) {
+      gen_expr(arg);
+      push();
+      nargs++;
+    }
+
+    for (int i = nargs - 1; i >= 0; i--)
+      pop(argreg[i]);
+
     printf("  call %s\n", node->funcname);
     return;
+  }
   }
 
   gen_expr(node->rhs);
