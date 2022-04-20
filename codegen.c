e5d849105fd7ae90992972c386d8d36be02d362a
ksco <numbksco@gmail.com>
2022-04-21 01:16:49 +0800

    Add global variables

--- codegen.c
+++ codegen.c
@@ -34,7 +34,15 @@ static int align_to(int n, int align) {
 static void gen_addr(Node *node) {
   switch (node->kind) {
   case ND_VAR:
-    printf("  addi a0,s0,%d\n", node->var->offset - node->var->ty->size);
+    if (node->var->is_local) {
+      // Local variable
+      printf("  addi a0,s0,%d\n", node->var->offset - node->var->ty->size);
+    } else {
+      // Global variable
+      printf("  lui a0,%%hi(%s)\n", node->var->name);
+      printf("  addi a0,a0,%%lo(%s)\n", node->var->name);
+    }
+
     return;
   case ND_DEREF:
     gen_expr(node->lhs);
@@ -207,9 +215,19 @@ static void assign_lvar_offsets(Obj *prog) {
   }
 }
 
-void codegen(Obj *prog) {
-  assign_lvar_offsets(prog);
+static void emit_data(Obj *prog) {
+  for (Obj *var = prog; var; var = var->next) {
+    if (var->is_function)
+      continue;
 
+    printf("  .data\n");
+    printf("  .globl %s\n", var->name);
+    printf("%s:\n", var->name);
+    printf("  .zero %d\n", var->ty->size);
+  }
+}
+
+static void emit_text(Obj *prog) {
   for (Obj *fn = prog; fn; fn = fn->next) {
     if (!fn->is_function)
       continue;
@@ -241,4 +259,10 @@ void codegen(Obj *prog) {
     printf("  addi sp,sp,%d\n", fn->stack_size + 16);
     printf("  ret\n");
   }
+}
+
+void codegen(Obj *prog) {
+  assign_lvar_offsets(prog);
+  emit_data(prog);
+  emit_text(prog);
 }
